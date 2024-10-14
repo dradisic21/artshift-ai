838b3b04c0bb799db2f7c97780f2b85685af59b6
Dino Radišić <radisic.dino@gmail.com>
2024-10-14 13:12:30 +0200

    remove error

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,21 +5,17 @@ import { Advantages } from "../../sections/Advantages/Advantages";
 import { Benefits } from "../../sections/Benefits/Benefits";
 import { Faq } from "../../sections/Faq/Faq";
 import { GetApp } from "../../sections/GetApp/GetApp";
-import { Subscribe } from "../../components/Subscribe/Subscribe";
-import "../../styles/HomePage.scss"
+import "../../styles/HomePage.scss";
 
 export function HomePage() {
   return (
-  
-      <div className="home-container">
-        <Header />
-        <Fetatures />
-        <Advantages />
-        <Benefits />
-        <Faq />
-        <GetApp />
-     
-     
+    <div className="home-container">
+      <Header />
+      <Fetatures />
+      <Advantages />
+      <Benefits />
+      <Faq />
+      <GetApp />
       <Footer />
     </div>
   );
