539ef60d083ac7e895a4ca851be808e4e09ddc4f
Dino Radišić <radisic.dino@gmail.com>
2024-10-15 10:54:55 +0200

    back to top button

--- src/components/BackToTopButton/BackToTopButton.jsx
+++ src/components/BackToTopButton/BackToTopButton.jsx
@@ -1,10 +1,19 @@
-import React, { useState, useEffect } from 'react';
-
-
-import { Button } from "../../ui/Button"
+import React, { useState, useEffect } from "react";
+import { Button } from "../../ui/Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowUp } from "@fortawesome/free-solid-svg-icons";
 
 export function BackToTopButton() {
   const [isVisible, setIsVisible] = useState(false);
+
+  const ArrowUpIcon = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+      <path
+        fill="white"
+        d="M246.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 109.3 361.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160zm160 352l-160-160c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L224 301.3 361.4 438.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3z"
+      />
+    </svg>
+  );
 
   useEffect(() => {
     const handleScroll = () => {
@@ -15,25 +24,26 @@ export function BackToTopButton() {
       }
     };
 
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth',
+      behavior: "smooth",
     });
   };
 
   return (
     <Button
-      className={`backToTopBtn ${isVisible ? 'visible' : 'hidden'}`}
+      className={`backToTopBtn ${isVisible ? "visible" : "hidden"}`}
       onClick={scrollToTop}
-    />
-   
+    >
+      <ArrowUpIcon />
+    </Button>
   );
-};
+}
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,7 +3,7 @@ import "../styles/Button.scss"
 export function Button(props) {
     return (
         <div>
-            <button className={props.className} type={props.type} onClick={props.onClick}>{props.name}</button>
+            <button className={props.className} type={props.type} onClick={props.onClick}>{props.name}{props.children}</button>
         </div>
     )
 }
